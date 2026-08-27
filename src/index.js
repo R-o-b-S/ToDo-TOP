88d781e0c1cd040c26bfa494f513fbc17dad53c3
R-o-b-S <189221924+R-o-b-S@users.noreply.github.com>
2026-08-27 08:53:56 +0200

    task UI blueprint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import "./styles.css";
-import { printList } from "./element.js";
+import { printList , list } from "./element.js";
 
 
 document.getElementById("printList").onclick = printList; //print the list of Tasks in console
 
+
+//expandes on hover and back to normal too long text in task notes
+const textBox = document.querySelector('.taskNotes');
+
+textBox.addEventListener('mouseenter', () => {
+  textBox.classList.add('expanded');
+});
+
+textBox.addEventListener('mouseleave', () => {
+  textBox.classList.remove('expanded');
+});
